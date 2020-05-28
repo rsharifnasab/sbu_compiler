0dd492fe403e3eca604fbe7b0f31cbda8442d333
roozbeh sharifnasab <rsharifnasab@gmail.com>
2020-05-29 03:49:30 +0430

    updated tests!

--- ll1_parser/src/Main.java
+++ ll1_parser/src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         System.out.println(parser.firstsToString());
         System.out.println(parser.followsToString());
         
-        System.err.println(parser.run());
+        System.out.println(parser.run());
 
     } // end main method 
 
